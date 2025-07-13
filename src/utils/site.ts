c7d7f1e276b611a590cd18abd7b4f6982ea1a46f
nakuwalar <nakuwalar@gmail.com>
2025-07-14 04:08:21 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Video Bokep Liadani';
-export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Smp Mama Viral Sma Cindo Tante China';
-export const url = 'https://videobokepliadani.pages.dev';
-export const terbit = '2025-06-23T00:01:03Z';
